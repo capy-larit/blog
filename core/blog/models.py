1023c09210be5870cb51ba568a51c62ed3f163a5
capy <larissa.laritt@icloud.com>
2022-09-26 17:04:11 -0300

    refactor: Remove datetime.

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from datetime import datetime
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
     conteudo = models.TextField()
-    data_publicacao = models.DateTimeField(default=datetime.now())
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
