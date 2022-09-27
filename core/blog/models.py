476ec2d611f533c44d810b54a671588e20401115
capy <larissa.laritt@icloud.com>
2022-09-27 09:33:53 -0300

    feat(ckeditor): Altera o tipo do atributo conteúdo p/ RichTextUploadingField().

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
-    conteudo = models.TextField()
+    conteudo = RichTextUploadingField()
     data_publicacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
