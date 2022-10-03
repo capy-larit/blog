2004f9f5b10535c2757e788593e2d943c9261dff
capy <larissa.laritt@icloud.com>
2022-10-03 16:18:48 -0300

    feat: Adiciona atributos resumo e imagem_capa.

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 class Post(models.Model):
     autor = models.CharField(max_length=255)
     titulo = models.CharField(max_length=255)
     subtitulo = models.CharField(max_length=255)
+    resumo = RichTextField(blank=True, null=True)
     conteudo = RichTextUploadingField()
-    data_publicacao = models.DateTimeField(auto_now_add=True)
+    imagem_capa = models.ImageField(null=True, blank=True, upload_to='static/blog/')
+    data_publicacao = models.DateTimeField(timezone.now())
 
     def __str__(self):
         return self.titulo
