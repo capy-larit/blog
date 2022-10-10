ea5dd10240c6391eec2891c3ee7a539b5a06238e
capy <larissa.laritt@icloud.com>
2022-10-10 11:17:15 -0300

    :sparkles: feat: Adiciona link, url, e funções.
    - Tag script para htmx.
    - Link para css global.
    - Cria tag para receber mensagem sobre o login.
    - Configura url para criar conta.
    - Cria template para logout.
    - Adiciona path para logout e redirect_home.
    - Cria funções processa_login, processa_logout e processa_redirect_home.

--- core/autenticacao/urls.py
+++ core/autenticacao/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-]
+    path('', views.processa_login, name='login'),
+    path('autenticacao/processa_logout/', views.processa_logout, name='logout'),
+    path('autenticacao/processa_redirect_home/', views.processa_redirect_home, name='ir_home'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
